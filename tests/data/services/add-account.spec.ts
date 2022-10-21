2b06028361f3cda2723fcf2f766a6f7d6b0068a4
Root1945 <munternl13@gmail.com>
2022-10-21 19:38:47 -0300

    add fake data

--- tests/data/services/add-account.spec.ts
+++ tests/data/services/add-account.spec.ts
@@ -2,13 +2,21 @@ import { AddAccountService } from '@/data/services'
 import { LoadUserAccountRepository } from '@/data/contracts/repos'
 
 import { mock } from 'jest-mock-extended'
+import { faker } from '@faker-js/faker'
+import { AddAccount } from '@/domain/features'
 
 describe('AddAccountService', () => {
   let sut: AddAccountService
   let loadUserAccountRepo: LoadUserAccountRepository
+  let fakeAddAccount: AddAccount.Params
 
   beforeAll(() => {
     loadUserAccountRepo = mock()
+    fakeAddAccount = {
+      name: faker.name.firstName(),
+      email: faker.internet.email(),
+      password: faker.internet.password()
+    }
   })
 
   beforeEach(() => {
@@ -16,13 +24,9 @@ describe('AddAccountService', () => {
   })
 
   it('should call LoadUserAccountRepo with correct params', async () => {
-    await sut.perform({
-      name: 'any_name',
-      email: 'any_email',
-      password: 'any_password'
-    })
+    await sut.perform(fakeAddAccount)
 
-    expect(loadUserAccountRepo.load).toHaveBeenCalledWith({ email: 'any_email' })
+    expect(loadUserAccountRepo.load).toHaveBeenCalledWith({ email: fakeAddAccount.email })
     expect(loadUserAccountRepo.load).toHaveBeenCalledTimes(1)
   })
 })
