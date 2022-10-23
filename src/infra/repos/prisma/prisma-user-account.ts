cf4877b2bcfa68815c76e99d50b36cde248f2ff5
Root1945 <munternl13@gmail.com>
2022-10-22 22:57:23 -0300

    implement LoadUserAccountRepository in PrismaUserAccount

--- src/infra/repos/prisma/prisma-user-account.ts
+++ src/infra/repos/prisma/prisma-user-account.ts
@@ -1,7 +1,7 @@
 import { LoadUserAccountRepository } from '@/data/contracts/repos'
 import prismaClient from '@/infra/repos/prisma/helpers/client'
 
-export class PrismaUserAccount {
+export class PrismaUserAccount implements LoadUserAccountRepository {
   async load (params: LoadUserAccountRepository.Params): Promise<LoadUserAccountRepository.Result> {
     const account = await prismaClient.user.findUnique({ where: { email: params.email } })
     if (account) return { id: account.id, name: account.name, email: account.email, password: account.password }
