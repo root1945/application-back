12cb59e918771165a03ade488d6c80ef41351120
Root1945 <munternl13@gmail.com>
2022-10-31 23:50:02 -0300

    ensure RequiredFields return MissingParamError if no value is provided

---
+++ tests/application/validation/required-fields.spec.ts
@@ -0,0 +1,29 @@
+
+class MissingParamError extends Error {
+  constructor (paramName: string) {
+    super(`Missing param: ${paramName}`)
+    this.name = 'MissingParamError'
+  }
+}
+
+class RequiredFields {
+  constructor (private readonly fieldNames: string[]) {}
+
+  validate (input: any): MissingParamError | undefined {
+    for (const field of this.fieldNames) {
+      if (!input[field]) {
+        return new MissingParamError(field)
+      }
+    }
+  }
+}
+
+describe('RequiredFields', () => {
+  it('should return MissingParamError if no value is provided', () => {
+    const sut = new RequiredFields(['any_field'])
+
+    const error = sut.validate({})
+
+    expect(error).toEqual(new MissingParamError('any_field'))
+  })
+})
