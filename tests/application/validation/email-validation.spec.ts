f71d457817939f6732387d50faa9024736570268
Root1945 <munternl13@gmail.com>
2022-11-13 18:50:04 -0300

    ensure EmailValidation returns error if isValid return false

--- tests/application/validation/email-validation.spec.ts
+++ tests/application/validation/email-validation.spec.ts
@@ -1,6 +1,7 @@
 import { EmailValidator } from '@/application/contracts'
 
 import { mock, MockProxy } from 'jest-mock-extended'
+import { InvalidParamError } from '@/application/errors'
 
 class EmailValidation {
   constructor (
@@ -8,8 +9,10 @@ class EmailValidation {
     private readonly emailValidator: EmailValidator
   ) {}
 
-  validate (): void {
-    this.emailValidator.isValid(this.value)
+  validate (): Error | undefined {
+    if (!this.emailValidator.isValid(this.value)) {
+      return new InvalidParamError('email')
+    }
   }
 }
 
@@ -30,5 +33,13 @@ describe('EmailValidation', () => {
 
     expect(emailValidator.isValid).toHaveBeenCalledWith('any_email')
     expect(emailValidator.isValid).toHaveBeenCalledTimes(1)
+  })
+
+  it('should return error if EmailValidator returns false', () => {
+    emailValidator.isValid.mockReturnValueOnce(false)
+
+    const error = sut.validate()
+
+    expect(error).toEqual(new InvalidParamError('email'))
   })
 })
