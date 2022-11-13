d07003b1b6fde51fe7953ef17490e7d1ae57f306
Root1945 <munternl13@gmail.com>
2022-11-13 19:19:02 -0300

    ensure ValidationBuilder return CompareFieldsValidation on compare())

--- tests/application/validation/builder.spec.ts
+++ tests/application/validation/builder.spec.ts
@@ -1,18 +1,22 @@
-import { RequiredFieldsValidation, Validator } from '@/application/validation'
+import { RequiredFieldsValidation, Validator, CompareFieldsValidation } from '@/application/validation'
 
 class ValidationBuilder {
   private constructor (
     private readonly value: any,
-    private readonly fieldNames: string[],
     private readonly validations: Validator[] = []
   ) {}
 
-  static of (params: { value: any, fieldNames: string[] }): ValidationBuilder {
-    return new ValidationBuilder(params.value, params.fieldNames)
+  static of (params: { value: any }): ValidationBuilder {
+    return new ValidationBuilder(params.value)
   }
 
-  required (): ValidationBuilder {
-    this.validations.push(new RequiredFieldsValidation(this.value, this.fieldNames))
+  required (fieldNames: string[]): ValidationBuilder {
+    this.validations.push(new RequiredFieldsValidation(this.value, fieldNames))
+    return this
+  }
+
+  compare (fieldName: string, fieldNameToCompare: string): ValidationBuilder {
+    this.validations.push(new CompareFieldsValidation(this.value, fieldName, fieldNameToCompare))
     return this
   }
 
@@ -27,12 +31,25 @@ describe('ValidationBuilder', () => {
       .of({
         value: {
           any_field: 'any_value'
-        },
-        fieldNames: ['any_field']
+        }
       })
-      .required()
+      .required(['any_field'])
       .build()
 
     expect(validators).toEqual([new RequiredFieldsValidation({ any_field: 'any_value' }, ['any_field'])])
+  })
+
+  it('should return CompareFieldsValidation', () => {
+    const validators = ValidationBuilder
+      .of({
+        value: {
+          field: 'any_value',
+          fieldToCompare: 'any_value'
+        }
+      })
+      .compare('field', 'fieldToCompare')
+      .build()
+
+    expect(validators).toEqual([new CompareFieldsValidation({ field: 'any_value', fieldToCompare: 'any_value' }, 'field', 'fieldToCompare')])
   })
 })
