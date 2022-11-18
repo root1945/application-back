96e10ce52fa1783edbea53ef9a626112d18a1189
Root1945 <munternl13@gmail.com>
2022-11-18 18:04:06 -0300

    add schema swagger to login

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -4,5 +4,78 @@ export default {
     title: 'Application API',
     description: 'API documentation for the application',
     version: '1.0.0'
+  },
+  servers: [{
+    url: '/api'
+  }],
+  tags: [{
+    name: 'Login'
+  }],
+  paths: {
+    '/login': {
+      post: {
+        tags: ['Login'],
+        summary: 'API to authenticate a user',
+        requestBody: {
+          content: {
+            'application/json': {
+              schema: {
+                $ref: '#/schemas/loginParams'
+              },
+              example: {
+                email: 'juliana@gmail.com',
+                password: 'juliana54'
+              }
+            }
+          }
+        },
+        responses: {
+          200: {
+            description: 'Success',
+            content: {
+              'application/json': {
+                example: {
+                  accessToken: 'any_token'
+                }
+              }
+            }
+          },
+          400: {
+            $ref: '#/components/badRequest'
+          },
+          401: {
+            $ref: '#/components/unauthorized'
+          },
+          500: {
+            $ref: '#/components/serverError'
+          }
+        }
+      }
+    }
+  },
+  schemas: {
+    loginParams: {
+      type: 'object',
+      properties: {
+        email: {
+          type: 'string'
+        },
+        password: {
+          type: 'string'
+        }
+      },
+      required: ['email', 'password']
+    }
+  },
+  components: {
+    badRequest: {
+      description: 'Invalid request'
+    },
+    unauthorized: {
+      description: 'Invalid credentials'
+    },
+    serverError: {
+      description: 'Internal server error'
+    }
   }
 }
